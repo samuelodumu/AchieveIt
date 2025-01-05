2e4af54b8b191f62cf82e7f1072977e17e82d709
Samuel Odumu <themainsamuel@gmail.com>
2025-01-05 19:03:07 +0100

    added icons and icreased some fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,17 @@
 import Timer from './Timer';
 import Quotes from './Quotes';
 import '../App.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faArrowRotateRight, faGear } from '@fortawesome/free-solid-svg-icons';
+library.add(faArrowRotateRight, faGear);
 
 function App() {
   return (
     <>
       <div className='app'>
-        <header>
-          <h1
-            className='yeseva-one-bold'
-            style={{ marginBottom: '0' }}>
-            AchieveIt
-          </h1>
-          <small className='yeseva-one-regular'>
+        <header className='mb-4'>
+          <h1 className='yeseva-one-bold display-3 mb-0 py-0'>AchieveIt</h1>
+          <small className='yeseva-one-regular mt-0 py-0'>
             Master your time, Achieve it all.
           </small>
           <hr />
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,6 @@
 // logic for the pomodoro timer
 import { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Timer() {
   const [time, setTime] = useState(25 * 60);
@@ -27,12 +28,24 @@ function Timer() {
   };
 
   return (
-    <div className='card py-5 px-5 mt-4' style={{ color: '#3c3c3e' }}>
+    <div
+      className='card py-5 px-5 mt-4'
+      style={{ color: '#3c3c3e' }}>
       <h6 className='pb-2'>Pomodoro | Short Break | Long Break</h6>
-      <h1 className='fw-bold time' style={{ fontSize: '120px'}}>{formatTime(time)}</h1>
+      <h1
+        className='fw-bold time'
+        style={{ fontSize: '120px' }}>
+        {formatTime(time)}
+      </h1>
       <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+        <FontAwesomeIcon
+          icon='fa-solid fa-arrow-rotate-right'
+          size='2x'
+          className='icons'
+          onClick={resetTimer}
+        />
         <button onClick={toggleTimer}>{isRunning ? 'Pause' : 'Start'}</button>
-        <button onClick={resetTimer}>Reset</button>
+        <FontAwesomeIcon icon='fa-solid fa-gear' size='2x' className='icons' />
       </div>
     </div>
   );
