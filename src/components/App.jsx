4ddd4345110ea872e23635ad52cfc55a4b941f37
Samuel Odumu <themainsamuel@gmail.com>
2025-01-10 03:15:27 +0100

    added text and footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,13 +37,13 @@ function App() {
   return (
     <>
       <div className='app'>
-        <header className='mb-4'>
-          <h1 className='yeseva-one-bold display-3 mb-0 py-0'>AchieveIt</h1>
-          <small className='yeseva-one-regular mt-0 py-0'>
-            Master your time, Achieve it all.
-          </small>
-        </header>
         <main>
+          <header className='mb-5'>
+            <h1 className='yeseva-one-bold display-3 mb-0'>AchieveIt</h1>
+            <small className='yeseva-one-regular mt-0'>
+              Master your time, Achieve it all.
+            </small>
+          </header>
           <Quotes />
           <hr />
           <div className='outer-div'>
@@ -67,6 +67,40 @@ function App() {
             )}
           </div>
         </main>
+        <section className='info-section container-fluid d-flex justify-content-center'>
+          <div className='col-md-5 text-start fancy-headers'>
+            <h2 className='pb-2'>About AchieveIt</h2>
+            <p>
+              AchieveIt is a productivity app designed to help you manage your
+              time effectively. Use the Pomodoro technique to break your work
+              into intervals, typically 25 minutes in length, separated by short
+              breaks. This method helps improve focus and productivity.
+            </p>
+            <p>
+              Customize your timer durations, enable or disable text shadow, and
+              choose whether to auto-start breaks all while drawing
+              encouragement from the affirmative messages displayed above the
+              timer. Achieve your goals with AchieveIt!
+            </p>
+            <h2 className='pb-2'>About the Pomodoro Technique</h2>
+            <p>
+              The Pomodoro Technique is created by Francesco Cirillo for a more
+              productive way to work and study. The technique uses a timer to
+              break down work into intervals, traditionally 25 minutes in
+              length, separated by short breaks. Each interval is known as a
+              pomodoro, from the Italian word for &apos;tomato&apos;, after the
+              tomato-shaped kitchen timer that Cirillo used as a university
+              student. -
+              <a href='https://en.wikipedia.org/wiki/Pomodoro_Technique'>
+                Wikipedia
+              </a>
+            </p>
+          </div>
+        </section>
+        <footer>
+          <p>Made with &lt;3 by <a href='https://x.com/SamuelOdumu'>Samuel Odumu</a></p>
+          <small>&copy; AchieveIt 2024</small>
+        </footer>
       </div>
     </>
   );
